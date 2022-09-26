216ada350ba4c58e782fb48ebba2311a18b0fcac
Sunil Kumar Majhi <msunil037@gmail.com>
2022-09-27 00:00:14 +0530

    statefull approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stateless-observable-pattern';
+  constructor(private _http: HttpClient){}
+  orgWebsiteUserDetails: any[] = [];
+  otherWebsiteUserDetails: any[] = [];
+  ngOnInit(){
+    this._http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any) => {
+      const users: any[] = res;
+      this.orgWebsiteUserDetails = users.filter(user => (user.website).includes('.org'));
+      this.otherWebsiteUserDetails = users.filter(user => !(user.website).includes('.org'));
+    })
+  }
 }
